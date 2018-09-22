6bb03c04319023c4830591646f9bf4bd8b8115d2
杨东雄 <2545493686@qq.com>
2018-09-22 21:08:00 +0800

    0.0.7 修复内存泄漏，优化性能

---
+++ DaySign/Script/Log/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaySign
+{
+    class Log : LogBase
+    {
+        static Log logObj = new Log();
+        public override string FilePath => "Log.txt";
+
+        static public void AddLog(string log)
+        {
+            logObj.AddLog(logObj.GetErrorClass(2), log);
+        }
+
+        public override void AddLog(ErrorClass errorClass, string log)
+        {
+            log = string.Format("{0} {1}.{2}:{3}", DateTime.Now.ToString(), errorClass.className, errorClass.frameName, log);
+            log.WriteLineToFile(FilePath);
+        }
+    }
+}
---
+++ DaySign/Script/Log/LogBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaySign
+{
+    struct ErrorClass
+    {
+        public string className; //类名
+        public string frameName; //方法名
+    }
+    abstract class LogBase
+    {
+        protected ErrorClass GetErrorClass(int depth)
+        {
+            StackTrace trace = new StackTrace();
+            StackFrame frame = trace.GetFrame(depth);
+            MethodBase method = frame.GetMethod();
+            string className = method.ReflectedType.Name;
+            ErrorClass ret = new ErrorClass()
+            {
+                className = className,
+                frameName = method.Name
+            };
+            return ret;
+        }
+
+        public abstract string FilePath { get; }
+
+        public abstract void AddLog(ErrorClass errorClass, string log);
+
+    }
+}
